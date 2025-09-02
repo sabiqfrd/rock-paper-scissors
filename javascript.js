4f39655511384adc6bf6ba0a917cda3061bf1dbf
sabiqfrd <sabiqfrd@gmail.com>
2025-09-02 23:51:17 +0800

    feat: Create a function for user's input

--- javascript.js
+++ javascript.js
@@ -13,21 +13,19 @@
 // 6. Declare game's winner of 5 rounds
     // a. Compare computer's and human's wins
 
-
-    
 // Create variables
-let rock = "Rock";
-let scissors = "Scissors";
-let paper = "Paper";
+let rock = "rock";
+let scissors = "scissors";
+let paper = "paper";
 
-// Create function getComputerChoice
+// Create function for computer's choice
 function getComputerChoice(){
     let computerChoice;
 
     // Generate random number between 1 to 10
     let randomNumber = Math.random() * 10;
 
-    // Assign number to variables
+    // Assign result to variables
     if (randomNumber <= 3){
         computerChoice = rock;
     } else if (randomNumber >= 7){
@@ -39,5 +37,40 @@ function getComputerChoice(){
     // Return computer's choice
     return computerChoice;
 }
+
+// Create function for human's choice
+function getHumanChoice(){
+    let isTrue = true;
+
+    while (isTrue){
+        // Ask human's choice
+        let getHumanChoice = prompt("Choose between rock, paper, or scissors.");
+
+        // Continue function when user's input is correct
+        isTrue = false;
+
+        let humanChoice;
+
+        // Assign result to variables
+        if (getHumanChoice.toLowerCase().trim() == rock){
+            humanChoice = rock;
+            console.log(humanChoice);
+        } else if (getHumanChoice.toLowerCase().trim() == paper){
+            humanChoice = paper;
+            console.log(humanChoice);
+        } else if (getHumanChoice.toLowerCase().trim() == scissors){
+            humanChoice = scissors;
+            console.log(humanChoice);
+        } else {
+            console.log("Please enter a valid value");
+            
+            // Repeat prompt when user's input is false
+            isTrue = true;
+        }
+
+    }
+}
+
+getHumanChoice();
 
 console.log(getComputerChoice());
